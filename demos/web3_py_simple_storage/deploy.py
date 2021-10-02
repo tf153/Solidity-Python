c6e923d64b8328a8ad436a8d9d3654765a1898d1
Rahul Joshi <tj18022826@gmail.com>
2021-10-02 20:44:06 +0530

    todays

---
+++ demos/web3_py_simple_storage/deploy.py
@@ -0,0 +1,18 @@
+from solcx import compile_standard
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outerSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+print(compiled_sol)
